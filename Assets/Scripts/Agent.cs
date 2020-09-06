24602fc1ad68c0e4d4b1e7a66ec8c771af30e047
cnr-adms <connor-adams@live.com>
2020-09-06 20:58:56 +0100

    Agent Classes

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public void MoveTo(Vector3 position)
+    {
+        // trigger new move request
+    }
+}
---
+++ Assets/Scripts/AgentManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    private List<Agent> ActiveAgents;
+
+    void Start()
+    {
+        ActiveAgents = new List<Agent>();
+
+        Agent newAgent = new Agent();
+        SpawnAgent(newAgent);
+    }
+
+    public void SpawnAgent(Agent agent)
+    {
+        ActiveAgents.Add(agent);
+    }
+}
