007678d7c5c559789845d1ea3560f3ba687bfd72
napedape <63455875+napedape@users.noreply.github.com>
2020-09-09 19:52:25 +0100

    Camera Zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float speed = 1.0f;
-    public float rotation = 10.0f;
+    public Camera sceneCamera;
+
+    // Movement and rotation variables
+    public float speed = 1.0f,
+                 rotation = 10.0f;
+
+    // Zooming variables
+    float zoomTime,
+          zoomTarget,
+          lastScrollWheelDirection;
+
+    public float scrollSpeed = 10.0f,
+                 yMin = -30.0f,
+                 yMax = 100.0f;
+
+    private float scrollWheel
+    {
+        get { return Input.GetAxis("Mouse ScrollWheel"); }
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        sceneCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         InputControls();
+
     }
 
     void InputControls()
@@ -55,5 +74,62 @@ public class CameraController : MonoBehaviour
 
             transform.Rotate(0.0f, rotation * Time.deltaTime, 0.0f, Space.World);
 
+        ZoomControl();
+
     }
+
+    void ZoomControl()
+    {
+        // Resets zoomTarget if direction of zoom direction changes
+        if ((lastScrollWheelDirection > 0 && scrollWheel < 0) ||
+            (lastScrollWheelDirection < 0 && scrollWheel > 0))
+        {
+            zoomTarget = 0;
+        }
+        if (scrollWheel != 0)
+        {
+            lastScrollWheelDirection = scrollWheel;
+        }
+
+        // zoomTarget defined. Amount left to be zoomed
+        zoomTarget += scrollWheel * scrollSpeed;
+
+        // zoomTime defined. Change in scrollWheel resets zoomTime, restarts .Lerp
+        if (scrollWheel != 0)
+        {
+            zoomTime = 0;
+        }
+
+        if (zoomTarget != 0)
+        {
+            zoomTime += Time.deltaTime;
+
+            // Calculating interpolation. scrollSpeed, or zoomTime's divisor can alter speed of zoom
+            float zoomInt = Mathf.Lerp(0, zoomTarget, zoomTime / 4.0f);
+            Vector3 translation = new Vector3(0, zoomInt, 0);
+
+            // Zoom the camera by the amount calculated this frame
+            transform.position -= translation;
+
+            // Decrease zoomTarget by amount moved this frame
+            zoomTarget -= translation.y;
+    
+        }
+
+        // Sets limits for zooming
+        var camPos = sceneCamera.transform.position.y;
+        var yRange = Mathf.Clamp(camPos, yMin, yMax);
+        
+        if (camPos < yMin || camPos > yMax)
+        {
+            sceneCamera.transform.position = new Vector3(sceneCamera.transform.position.x, yRange, sceneCamera.transform.position.z);
+        }
+    }
+
+    void DefaultCameraPosition ()
+    {
+        sceneCamera.transform.localPosition = Vector3.zero;
+    }
+
+    
 }
